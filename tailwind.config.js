23c09794c33bf1137a559f6c238645c85d4c5079
Pain0969 <6pathsofpain0969@gmail.com>
2023-07-05 21:42:05 +0530

    Razorpay clone

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+    fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#49b4ed",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBluehead: "#162f56",
+      gray2: "525a76",
+  },},
+},
+  plugins: [],
+}
